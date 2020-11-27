78d9323c8421e81de2b2c702b6bed89d6806b7d8
Simon Galvis <simongalvis9@gmail.com>
2020-11-27 13:19:42 -0700

    Modify migration 001 and add seeds folder

---
+++ seeds/seed.noteful_folder.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes (title, content)
+VALUES
+('First note!', 'First note content!'),
+('Second note!', 'Second note content!'),
+('Third note!', 'Third note content!'),
+('Fourth note!', 'Fourth note content!'),
+('Fith note!', 'Fith note content!');
